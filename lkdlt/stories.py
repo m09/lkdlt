5d99f33d68ad63be413d42efce28af9eb004a6c2
m09 <142691+m09@users.noreply.github.com>
2023-03-17 11:10:36 +0800

    Update stories HTML conversion
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- lkdlt/stories.py
+++ lkdlt/stories.py
@@ -1,3 +1,4 @@
+from re import IGNORECASE
 from re import compile as re_compile
 from re import sub
 
@@ -9,6 +10,10 @@ _ref_pattern = re_compile(r"\{([^{}]+)\}")
 def process_story_to_html(keyword: str, story: str) -> str:
     story = _component_pattern.sub(r"<i>\1</i>", story)
     story = _ref_pattern.sub(r"(\1)", story)
-    story = sub(rf"\b{keyword}\b", f"<b>{keyword}</b>", story)
-    story = _keyword_pattern.sub(r"<b>\1</b>", story)
+    new_story = _keyword_pattern.sub(r"<b>\1</b>", story)
+    story = (
+        sub(rf"\b{keyword}\b", f"<b>{keyword}</b>", story, flags=IGNORECASE)
+        if new_story == story
+        else new_story
+    )
     return story
