55e66f63049033839edd83fa7180efbca5ea9ad8
m09 <142691+m09@users.noreply.github.com>
2023-03-17 13:32:24 +0800

    Improve story to HTML conversion again
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- lkdlt/stories.py
+++ lkdlt/stories.py
@@ -8,11 +8,16 @@ _ref_pattern = re_compile(r"\{([^{}]+)\}")
 
 
 def process_story_to_html(keyword: str, story: str) -> str:
-    story = _component_pattern.sub(r"<i>\1</i>", story)
+    story = _component_pattern.sub(r'<span class="composant">\1</span>', story)
     story = _ref_pattern.sub(r"(\1)", story)
-    new_story = _keyword_pattern.sub(r"<b>\1</b>", story)
+    new_story = _keyword_pattern.sub(r'<span class="motclef">\1</span>', story)
     story = (
-        sub(rf"\b{keyword}\b", f"<b>{keyword}</b>", story, flags=IGNORECASE)
+        sub(
+            rf"\b{keyword}\b",
+            f'<span class="motclef">{keyword}</span>',
+            story,
+            flags=IGNORECASE,
+        )
         if new_story == story
         else new_story
     )
